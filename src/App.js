7866a41bbf8557e028043ae9c2ac5cc467ca26c8
Jennifer <jennifertorchelsen@gmail.com>
2023-01-25 21:01:38 -0300

    Atualizando código

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Banner from './componente/Banner';
 import Formulario from './componente/Formulario';
 
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
--- src/componente/CampoTexto/index.js
+++ src/componente/CampoTexto/index.js
@@ -1,18 +1,27 @@
-import './CampoTexto.css'
 
-
-const CampoTexto = (props) => {
     //PROPS = Armazena as propriedades que esse componente recebeu(campo texto)
     //COMPONENTE Campo_Texto
 
-    const placeholderModificador = `${props.placeholder}...`
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...` 
+    //Dúvida
+    
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+    //Dúvida
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder= {placeholderModificador} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
+
 export default CampoTexto
