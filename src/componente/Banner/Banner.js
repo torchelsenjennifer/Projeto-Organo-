915e297bf7a9e9f2ae8d86854f18d2fab6687063
Jennifer <jennifertorchelsen@gmail.com>
2023-01-29 23:01:04 -0300

    atualizando link do banner

--- src/componente/Banner/Banner.js
+++ src/componente/Banner/Banner.js
@@ -1,9 +1,10 @@
 import './Banner.css'
+// import logo from '../../../public/Imagens/banner.png'
 //COMPONENTE 
 export const Banner = () =>{
     return (
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 
