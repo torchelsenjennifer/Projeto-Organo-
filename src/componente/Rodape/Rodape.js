8cbaf75ae39b2436cb7866edc08e101998b542c1
Jennifer <jennifertorchelsen@gmail.com>
2023-01-31 11:48:44 -0300

    Atualizando rodape

--- src/componente/Rodape/Rodape.js
+++ src/componente/Rodape/Rodape.js
@@ -1,25 +1,64 @@
 import './Rodape.css'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
+
 
 //COMPONENTE 
 export const Rodape = () =>{
     
-    return (
-            <div className='rodape'>
+//     return (
+//             <div className='rodape'>
+//                 <div className='iconeF'>
+//                         {/* <a href='https://pt-br.facebook.com/AluraCursosOnline/'>
+//                                  <img src={process.env.PUBLIC_URL + "/imagens/fb.png"}></img> 
+//                         </a> */}
+//                         <img src={process.env.PUBLIC_URL + "/imagens/fb.png"} onClick={()=> window.open("https://pt-br.facebook.com/AluraCursosOnline/")}></img>
+//                         <a href='https://pt-br.facebook.com/AluraCursosOnline/'>
+//                                  <h3>{process.env.PUBLIC_URL + "/imagens/fb.png"}</h3> 
+//                         </a>
+//                 </div>
+
+//                 <div className='iconeT'>
+//                         <img src="/imagens/tw.png" alt="logo twitter" />  
+//                 </div>
+//                 <div className='iconeI'>
+//                         <img  src="/imagens/ig.png"  alt="logo instagram" />  
+//                 </div>
+//             </div>
+            
+        
+//     )
+
+return(
+        
+        <footer className='rodape'>
                 <div className='iconeF'>
                         <a href='https://pt-br.facebook.com/AluraCursosOnline/'>
-                                 <img src={process.env.PUBLIC_URL + "/imagens/fb.png"}></img> 
+                                <FaFacebook size={50}/>
                         </a>
                 </div>
                 <div className='iconeT'>
-                        <img src="/imagens/tw.png" alt="logo twitter" />  
+                        <a href='https://twitter.com/AluraOnline'>
+                                <FaTwitter size={50}/>
+                        </a>
                 </div>
                 <div className='iconeI'>
-                        <img  src="/imagens/ig.png"  alt="logo instagram" />  
+                        <a href='https://www.instagram.com/aluraonline/'>
+                                <FaInstagram size={50}/>
+                        </a>
                 </div>
-            </div>
-            
-        
-    )
+{/* 
+                <div className='logo'>
+                        <img src={process.env.PUBLIC_URL + "/imagens/logo.png"}></img>
+                </div>
+
+                
+                <h3 className='assinatura'>Desenvolvido por alura.</h3> */}
+               
+
+              
+                
+        </footer>
+)
 
 }
 
